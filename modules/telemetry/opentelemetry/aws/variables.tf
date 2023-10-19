625f9a267e5598a4013cb24de93466f6dca6785a
Tony Phan <tphan@hashicorp.com>
2023-10-19 13:11:41 +1100

    feat: Add opentelemetry

---
+++ modules/telemetry/opentelemetry/aws/variables.tf
@@ -0,0 +1,24 @@
+variable "deployment_name" {
+  description = "deployment name to prefix resources"
+  type        = string
+}
+
+variable "namespace" {
+  description = "kubernetes namespace"
+  type        = string
+}
+
+variable "helm_chart_version" {
+  description = "helm chart version"
+  type        = string
+}
+
+variable "splunk_hec_endpoint" {
+  description = "splunk hec endpoint"
+  type        = string
+}
+
+variable "splunk_hec_token" {
+  description = "splunk hec token"
+  type        = string
+}
